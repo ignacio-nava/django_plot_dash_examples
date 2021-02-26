bab9f36a26364c0da2154ac772094f8c4070ed0f
ignacio-nava <ignan_90@hotmail.com>
2021-02-26 09:10:41 -0300

    Change context workflow

--- django_pd_examples/plots/views.py
+++ django_pd_examples/plots/views.py
@@ -4,9 +4,10 @@ from plots_examples import examples
 
 def exampleDetailView(request, example):
     template_name = 'plots/plot_detail.html'
-    reference = getattr(examples, example).reference
+    plot_example = getattr(examples, example)
     ctx = {
         'example': example,
-        'reference': reference
+        'reference': plot_example.reference,
+        'ratio': plot_example.ratio
         }
     return render(request, template_name, ctx)
